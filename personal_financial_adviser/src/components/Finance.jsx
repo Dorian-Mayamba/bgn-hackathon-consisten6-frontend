e0bad25bf18af8680c0b24a8a3035bf0d040c397
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-10-07 18:09:43 +0100

    added new style files

--- personal_financial_adviser/src/components/Finance.jsx
+++ personal_financial_adviser/src/components/Finance.jsx
@@ -1,7 +1,20 @@
 export function Finance(){
     return (
         <div>
-            <h1>Manage your finance here!!!</h1>
+            <h1 className="text-center">Manage your finance here!!!</h1>
+            <form action="#">
+                <div className="form-group row w-100">
+                    <div className="col-md-6">
+                        <input type="number" className="form-control" placeholder="You age"/>
+                    </div>
+                    <div className="col-md-6">
+                        <input type="number" className="form-control" name="monthly_incomes" id="monthly_incomes" placeholder="Your monthly incomes"/>
+                    </div>
+                </div>
+            </form>
+            <div className="financial-goals-section">
+                <h1><small>Your financial goals</small></h1>
+            </div>
         </div>
     )
 }
