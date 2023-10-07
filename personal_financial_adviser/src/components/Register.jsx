9c1e774bb6d7ba0e2decd8e2f5c1649eea28d5c8
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-10-07 17:43:18 +0100

    fixed register page

--- personal_financial_adviser/src/components/Register.jsx
+++ personal_financial_adviser/src/components/Register.jsx
@@ -1,30 +1,35 @@
-export function Register(){
+export function Register() {
     return (
         <div>
             <h1>Register</h1>
             <form action="/action_page.php">
-                <div>
+                <div className="form-group">
                     <label for="forename">First Name:</label>
                     <input type="name" class="form-control" id="fname" placeholder="Enter First Name" name="fname"></input>
                 </div>
-                <div>
+                <div className="form-group">
                     <label for="surname">Last Name:</label>
                     <input type="name" class="form-control" id="lname" placeholder="Enter Last Name" name="lname"></input>
                 </div>
-                <div>
+                <div className="form-group">
                     <label for="Email">Email:</label>
                     <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"></input>
                 </div>
-                <div>
+                <div className="form-group">
+                    <label for="age">Your age:</label>
+                    <input className="form-control" type="number" name="age" id="age" placeholder="enter your age" />
+                </div>
+                <div className="form-group">
                     <label for="pwd">Password:</label>
                     <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"></input>
                 </div>
-                <div>
-                    <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="remember"> Remember me </input>
-                    </label>
+                <div className="form-group">
+                    <label for="pwd">Confirm your Password:</label>
+                    <input type="password" class="form-control" id="pwd_confirm" placeholder="Confirm your password" name="pswd_confirm"></input>
                 </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+                <div className="form-group">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </form>
         </div>
     )
