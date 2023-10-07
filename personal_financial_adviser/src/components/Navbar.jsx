93f04aff25b94a2ea9ec8075d30a6b00b86c22b6
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-10-07 15:41:05 +0100

    added bootstrap

--- personal_financial_adviser/src/components/Navbar.jsx
+++ personal_financial_adviser/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 export function Navbar(){
     return(
-        <nav>
-            <ul>
+        <nav className="navbar navbar-expand-md text-light bg-primary">
+            <ul className="nav">
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link className="text-light" to='/'>Home</Link>
                 </li>
                 <li>
-                    <Link to='/finance'>Finance</Link>
+                    <Link className="text-light" to='/finance'>Finance</Link>
                 </li>
             </ul>
         </nav>
